660b06a764c5033b19c61465fdd38ed2f904b783
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-09-21 16:36:19 +0100

    fixes to build for pyodide standalone build

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import contextlib
 from glob import glob
-from distutils.core import setup,Extension
+from setuptools import setup,Extension
 
-#from distutils.command.build import build
-from distutils.command.build_ext import build_ext
+import setuptools
+from numpy import get_include
+from setuptools.command.build import build
+from setuptools.command.build_ext import build_ext 
 import os
-from numpy.distutils.misc_util import get_numpy_include_dirs
 
 @contextlib.contextmanager
 def changed_dir(dirname):
@@ -18,15 +19,25 @@ def changed_dir(dirname):
     finally:
         os.chdir(oldcwd)
 
-class launch_cmake(build_ext):
+class cmake_then_build(build):
     def run(self):
+        print("RUNNING BUILD CMD")
         self.run_cmake()
+        print("WOO")
+        build.run(self)
 
     def run_cmake(self):
-        numpy_include_folder=get_numpy_include_dirs()[0]
+        print("RUNNING CMAKE")
         with changed_dir("build"):
-            self.spawn(['cmake','..',f'-DNUMPY_INCLUDE={numpy_include_folder}'])
+            numpy_include_folder=get_include()
+            self.spawn(['cmake','..',f'-DNUMPY_INCLUDE={numpy_include_folder}','-DCMAKE_MAKE_PROGRAM=make'])
             self.spawn(['cmake','--build','.','-j','16'])
+
+
+# this is just so that it builds as an extension specific to pyodide/emscripten
+class do_nothing_ext(build_ext):
+    def run(self):
+        pass
 
 
 # make empty packages for things created during cmake or else bad things might happen
@@ -38,10 +49,7 @@ def make_empty_package(name):
         with open(init_file,'w') as fp:
             fp.write("\n")
 
-#make_empty_package("parquet")
 make_empty_package("vendored")
-
-
 
 setup(name='pyarrow',
       version='7.0',
@@ -50,9 +58,9 @@ setup(name='pyarrow',
       author_email='joe.marshall@nottingham.ac.uk',
       url='https://www.cs.nott.ac.uk/~pszjm2/',
       include_package_data=True,
-      ext_modules = [Extension("ignored", [""])],
+      ext_modules = [Extension("ignored", [])],
       packages=['pyarrow','pyarrow.vendored'],
       package_dir = { 'pyarrow' : 'build/pyodide/pyarrow','pyarrow.vendored':'build/pyodide/pyarrow/vendored'},
-      cmdclass={'build_ext':launch_cmake},
+      cmdclass={'build_ext':do_nothing_ext,'build':cmake_then_build},
       package_data={'':["*.so"]}
      )
